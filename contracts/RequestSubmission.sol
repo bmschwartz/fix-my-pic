104dc7ad7fa32cdf0ec5f958dfe7ebb29c35f94d
Ben Schwartz <schwartz.ben0@gmail.com>
2024-07-20 07:51:38 -0400

    next

--- contracts/RequestSubmission.sol
+++ contracts/RequestSubmission.sol
@@ -2,27 +2,44 @@
 pragma solidity ^0.8.0;
 
 contract RequestSubmission {
-  string public imageId;
+  string public watermarkedPictureUrl;
+  string public encryptedPictureUrl;
   string public description;
   uint256 public price;
   address public pictureRequest;
-  address payable public submitter;
+  address public submitter;
+
+  event RequestCreated(
+    address indexed submitter,
+    string description,
+    uint256 price,
+    string watermarkedPictureUrl,
+    string encryptedPictureUrl
+  );
 
   constructor(
+    uint256 _price,
     address _submitter,
     string memory _description,
-    string memory _imageId,
-    uint256 _price
+    string memory _watermarkedPictureUrl,
+    string memory _encryptedPictureUrl
   ) {
     require(_price >= 0, 'Price must be positive or zero');
     require(_submitter != address(0), 'Submitter address cannot be zero');
 
-    submitter = payable(_submitter);
+    submitter = _submitter;
     description = _description;
-    imageId = _imageId;
     price = _price;
+    watermarkedPictureUrl = _watermarkedPictureUrl;
+    encryptedPictureUrl = _encryptedPictureUrl;
     pictureRequest = msg.sender;
-  }
 
-  receive() external payable {}
+    emit RequestCreated(
+      _submitter,
+      _description,
+      _price,
+      _watermarkedPictureUrl,
+      _encryptedPictureUrl
+    );
+  }
 }
