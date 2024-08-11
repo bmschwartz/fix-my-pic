2db9cf74ed96778712e40b835869f2d620193db7
Ben Schwartz <schwartz.ben0@gmail.com>
2024-08-10 20:25:57 -0400

    Deployed factory and price contracts

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    tsconfigPath: path.resolve(__dirname, 'tsconfig.build.json'),
+  },
   reactStrictMode: true,
   images: {
     remotePatterns: [
