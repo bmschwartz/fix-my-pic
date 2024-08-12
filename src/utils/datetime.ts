1682661d9221df14ad9fd9c4d2a7c8e6dfdd6b3f
Ben Schwartz <schwartz.ben0@gmail.com>
2024-08-11 21:07:03 -0400

    fix(datetime): correctly display date for comment

--- src/utils/datetime.ts
+++ src/utils/datetime.ts
@@ -3,3 +3,14 @@ export function getUnixTimestampOneYearFromNow(): number {
   const currentTimestamp = Math.floor(Date.now() / 1000); // Current Unix timestamp in seconds
   return currentTimestamp + oneYearInSeconds;
 }
+
+export function getDateTimeFromUnixTimestamp(timestamp: number): string {
+  const date = new Date(timestamp * 1000); // multiply by 1000 to convert from seconds to milliseconds
+  return date.toLocaleString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+}
