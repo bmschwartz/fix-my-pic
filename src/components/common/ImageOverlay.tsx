0d32e2bc71ea77f4962e70e352a1d88f12d025b6
Ben Schwartz <schwartz.ben0@gmail.com>
2024-08-11 21:42:50 -0400

    feat(css): css changes in ImageOverlay

--- src/components/common/ImageOverlay.tsx
+++ src/components/common/ImageOverlay.tsx
@@ -65,11 +65,11 @@ const ImageOverlay: React.FC<ImageOverlayProps> = ({ imageUrl, onClose, onDownlo
           <Image src={imageUrl} alt="Overlay Image" layout="fill" objectFit="contain" style={{ borderRadius: '8px' }} />
         </Box>
         {description && (
-          <FMPTypography variant="h6" sx={{ mt: 1, color: 'white', textAlign: 'center' }}>
+          <FMPTypography variant="h6" sx={{ mt: 1, mx: 2, maxWidth: 500, color: 'white', textAlign: 'center' }}>
             {description}
           </FMPTypography>
         )}
-        <Box sx={{ mt: 1, display: 'flex', gap: 2 }}>
+        <Box sx={{ mt: 1, mb: 3, display: 'flex', gap: 2 }}>
           <FMPButton variant="contained" color="primary" onClick={onDownload}>
             {price === undefined || price === 0 ? 'Download' : `Buy for $${price}`}
           </FMPButton>
