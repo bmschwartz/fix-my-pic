ca3f38c6252b5be52611a4c427ecddae764d39b7
Ben Schwartz <schwartz.ben0@gmail.com>
2024-08-17 18:09:40 -0400

    feat(mint): all working now

--- contracts/FixMyPicNFT/BaseFixMyPicNFT.sol
+++ contracts/FixMyPicNFT/BaseFixMyPicNFT.sol
@@ -2,35 +2,33 @@
 pragma solidity ^0.8.0;
 
 import '@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol';
-import '@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol';
+import '@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol';
-import '@openzeppelin/contracts/utils/Counters.sol';
 
-contract BaseFixMyPicNFT is Initializable, ERC721Upgradeable, OwnableUpgradeable {
-  using Counters for Counters.Counter;
-  Counters.Counter private _tokenIds;
-
-  // Mapping from token ID to IPFS hash (or any other metadata)
-  mapping(uint256 => string) private _tokenURIs;
+contract BaseFixMyPicNFT is Initializable, ERC721URIStorageUpgradeable, OwnableUpgradeable {
+  uint256 private _tokenIdCounter;
 
   function initialize() public initializer {
     __ERC721_init('FixMyPicNFT', 'FMPNFT');
     __Ownable_init();
+    _tokenIdCounter = 1; // Start token IDs from 1 for clarity
   }
 
   function mintNFT(address _recipient, string memory _tokenURI) public onlyOwner returns (uint256) {
-    _tokenIds.increment();
-    uint256 newItemId = _tokenIds.current();
-    _mint(_recipient, newItemId);
+    uint256 newItemId = _tokenIdCounter;
+    _tokenIdCounter += 1;
+
+    _safeMint(_recipient, newItemId);
     _setTokenURI(newItemId, _tokenURI);
+
     return newItemId;
   }
 
-  function _setTokenURI(uint256 _tokenId, string memory _tokenURI) internal virtual {
-    _tokenURIs[_tokenId] = _tokenURI;
+  function _burn(uint256 tokenId) internal virtual override(ERC721URIStorageUpgradeable) {
+    super._burn(tokenId);
   }
 
-  function tokenURI(uint256 _tokenId) public view virtual override(ERC721Upgradeable) returns (string memory) {
-    return _tokenURIs[_tokenId];
+  function tokenURI(uint256 tokenId) public view virtual override(ERC721URIStorageUpgradeable) returns (string memory) {
+    return super.tokenURI(tokenId);
   }
 }
